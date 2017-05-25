3cac3a8560a1a62775fb889cf949ecc6bd754ba2
Stewart Ainsworth <stewart.ainsworth@nice.org.uk>
2017-05-25 11:59:29 +0100

    calling API ping

--- Local.FSharp.Web/Scripts/OntologyTypeahead/Main.js
+++ Local.FSharp.Web/Scripts/OntologyTypeahead/Main.js
@@ -1,3 +1,28 @@
 ﻿$('#btnPing').click(function () {
-    swal("Good job!", "You clicked the button!", "success")
+    var start = Date.now();
+    $.ajax({
+        url: $APIRoot_ontologyTypeahead + '/admin/ping',
+        dataType: 'json',
+        success: function(data){
+            swal({
+                title: "Ping Success",
+                text: "ping responded : " + (Date.now() - start) + "ms",
+                type: "success"
+            })
+        },
+        error: function (xhr, textStatus, errorThrown) {
+            swal({
+                title: "Ping Error",
+                text: errorThrown,
+                type: "error"
+            })
+        //},
+        //complete: function (xhr, textStatus) {
+        //    swal({
+        //        title: "Ping Response",
+        //        text: textStatus,
+        //        type: "warning"
+        //    })
+        }
+    });
 });
